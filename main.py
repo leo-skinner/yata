2971c8e633d65e9400aa5cdb50387fa59e72ad40
Leo Skinner <leoskinner@gmail.com>
2019-11-27 19:03:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import mensagens
+ 
+mensagens.welcome()
+mensagens.loser()
+mensagens.winner()
---
+++ mensagens.py
@@ -0,0 +1,11 @@
+#mensagens do jogo
+def welcome():
+    print("*"*37)
+    print("*Y A T A: Yet Another Text Adventure*")
+    print("*"*37)
+
+def winner():
+    print("Parabéns! Você chegou ao final do labirinto.")
+
+def loser():
+    print("Você perdeu. Tente outra vez.")
